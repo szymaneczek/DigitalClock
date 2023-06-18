930a6395434d45cb9f702802e8d2dacde8320d6f
szymaneczek <szymaneczek@gmail.com>
2023-06-18 16:47:43 +0200

    added a day

--- MyFrame.java
+++ MyFrame.java
@@ -6,8 +6,11 @@ import java.util.Calendar;
 public class MyFrame extends JFrame {
 
     SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
     JLabel timeLabel;
+    JLabel dayLabel;
     String time;
+    String day;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,12 +19,17 @@ public class MyFrame extends JFrame {
         this.setSize(350,500);
 
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
         timeLabel = new JLabel();
         timeLabel.setFont(new Font("Arial",Font.PLAIN,50));
         timeLabel.setForeground(new Color(0x13E313));
         timeLabel.setBackground(Color.BLACK);
         timeLabel.setOpaque(true);
+
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Arial",Font.PLAIN,25));
         this.add(timeLabel);
+        this.add(dayLabel);
         this.setVisible(true);
 
         setTime();
@@ -31,6 +39,9 @@ public class MyFrame extends JFrame {
         while (true){
             time = timeFormat.format(Calendar.getInstance().getTime());
             timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
 
             try{
                 Thread.sleep(1000);
